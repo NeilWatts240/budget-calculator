5549e74b09c12adab9c99fd23580cc24564f71dd
Maksim Vasilenko <vasilenko.m96@gmail.com>
2022-10-24 04:41:33 +0300

    lesson2

---
+++ script.js
@@ -0,0 +1,21 @@
+let money = 5000;
+let income = 'freelance';
+let addExpenses = 'интернет, такси, коммуналка';
+let deposit = true;
+let mission = 1000000;
+let period = 6;
+let budgetDay = money / 30;
+
+// lesson01
+// alert('ku');
+
+// lesson02
+// console.log(typeof money);
+// console.log(typeof income);
+// console.log(typeof deposit)
+// console.log(addExpenses.length);
+// console.log('Период равен ' + period + ' месяцев');
+// console.log('Цель заработать ' + mission + ' рублей');
+addExpenses.toLowerCase();
+console.log(addExpenses.split(', '));
+console.log(budgetDay);
